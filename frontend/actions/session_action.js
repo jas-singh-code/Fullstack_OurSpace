a7e531864130f0b9513de7c95fe635f0603cf06a
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-03-16 18:34:19 -0400

    finished session_actions and util

--- frontend/actions/session_action.js
+++ frontend/actions/session_action.js
@@ -1,8 +1,8 @@
 import * as APIUtil from '../util/session_api_util';
 
-const RECEIVE_SESSION_ERRORS ='RECEIVE_SESSION_ERRORS';
-const RECEIVE_CURRENT_USER ='RECEIVE_CURRENT_USER';
-const LOGOUT_CURRENT_USER ='LOGOUT_CURRENT_USER';
+export const RECEIVE_SESSION_ERRORS ='RECEIVE_SESSION_ERRORS';
+export const RECEIVE_CURRENT_USER ='RECEIVE_CURRENT_USER';
+export const LOGOUT_CURRENT_USER ='LOGOUT_CURRENT_USER';
 
 export const receiveCurrentUser = currentUser => ({
     type: RECEIVE_CURRENT_USER,
