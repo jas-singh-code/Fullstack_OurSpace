ea79edafac466bf16d333fdba33103fe4a3d619e
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-03-16 16:50:17 -0400

    added to session_actions

--- frontend/actions/session_action.js
+++ frontend/actions/session_action.js
@@ -3,5 +3,26 @@ import * as APIUtil from '../util/session_api_util';
 const RECEIVE_SESSION_ERRORS ='RECEIVE_SESSION_ERRORS';
 const RECEIVE_CURRENT_USER ='RECEIVE_CURRENT_USER';
 const LOGOUT_CURRENT_USER ='LOGOUT_CURRENT_USER';
+const CLEAR_ERRORS = 'CLEAR_ERRORS';
 
+export const receiveCurrentUser = currentUser => ({
+    type: RECEIVE_CURRENT_USER,
+    currentUser
+})
 
+export const logoutCurrentUser = () => ({
+    type: LOGOUT_CURRENT_USER,
+})
+
+export const receiveErrors = errors => ({
+    type: RECEIVE_SESSION_ERRORS,
+    errors
+})
+
+export const signup = user => dispatch => (
+    APIUtil.signup(user).then(user => (
+        dispatch(receiveCurrentUser(user))
+    ), err => (
+        dispatch(receiveErrors(err.responseJSON))
+    ))
+);
