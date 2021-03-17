0ed75e64ab4e7ad55838c0fecd74e2760eb9b3a8
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-03-17 09:55:31 -0400

    changed name in packagejson

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 // import { Provider } from 'react-redux';
-import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
+// import { Route, Redirect, Switch, Link, HashRouter } from 'react-router-dom';
 
 const App = () => {
     <div className="App">
-        <Switch>
-
-        </Switch>
+     
     </div>
 }
 
