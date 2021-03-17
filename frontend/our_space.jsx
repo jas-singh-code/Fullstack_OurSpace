a8dfe473b681c6a9ad5d709e082dbf89c9d95a4b
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-03-17 18:48:42 -0400

    added script to app.html and entry file to allw user info to persist after reload

--- frontend/our_space.jsx
+++ frontend/our_space.jsx
@@ -5,7 +5,28 @@ import Root from './components/root';
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    const store = configureStore();
+    let store;
+    if (window.currentUser) {
+      const { currentUser } = window;
+      const { id } = currentUser;
+      const preloadedState = { 
+        entitities: {
+          users: {
+            [id]: currentUser
+          }
+        },
+        session: { id }
+        };
+      store = configureStore(preloadedState);
+  
+      // Clean up after ourselves so we don't accidentally use the
+      // global currentUser instead of the one in the store
+      delete window.currentUser;
+  
+    } else {
+      store = configureStore();
+    }
+  
     const root = document.getElementById('root');
     ReactDOM.render(<Root store={store} />, root);
 })
