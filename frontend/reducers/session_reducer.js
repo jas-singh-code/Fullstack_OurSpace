9785dcc61e454116043567f49d8a9f244b821476
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-03-17 11:06:13 -0400

    finished sessions and errors reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,21 @@
+import { 
+    LOGOUT_CURRENT_USER,
+     RECEIVE_CURRENT_USER
+    } from "../actions/session_action";
+
+const _nullSession = {
+    currentUserId: null
+}
+const sessionReducer = (state=_nullSession, action) => {
+    Object.freeze(state);
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return {currentUserId: action.currentUser.id}
+        case LOGOUT_CURRENT_USER:
+            return _nullSession;
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
