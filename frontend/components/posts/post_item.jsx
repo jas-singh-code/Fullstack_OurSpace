e430a406cb00c20dfd944a59745af83e2cd9e575
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-04-21 21:05:50 -0400

    added to module-holder

--- frontend/components/posts/post_item.jsx
+++ frontend/components/posts/post_item.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {AiOutlineLike} from "react-icons/ai"
+import {FaRegCommentAlt} from "react-icons/fa"
 
 export default ({message, author, photoURL, createdAt}) => {
     let image;
@@ -10,6 +12,21 @@ export default ({message, author, photoURL, createdAt}) => {
             <li className="post-ltem-li">
                 <span>{author.first_name}, {createdAt}</span>
                 <p>{message}</p>
+                <div className="photos"></div>
+                <div className='module-holder'>
+                    <span>
+                        <div className="likes-btn">
+                            <AiOutlineLike className="like-icon" size="0.7g"/>
+                            <p>Like</p>
+                        </div>
+                    </span>
+                    <span>
+                        <div className="comments-btn">
+                            <FaRegCommentAlt className="comment-icon" size="0.7g"/>
+                            <p>Comment</p>
+                        </div>
+                    </span>
+                </div>
             </li>
         </div>
     )
