b83fe0ffe90adf9643f6c275f9e8b8955a63b9e2
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-05-08 07:30:11 -0400

    add friendship and friendreq reducer to entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,11 +3,14 @@ import posts from "./post_reducer";
 import comments from "./comments_reducer";
 import likes from "./likes_reducer";
 import users from "./users_reducer";
+import friendRequests from './friend_request_reducer';
+import friendships from './friendship_reducer';
 
 export default combineReducers ({
     users,
     posts,
     likes, 
     comments,
-    // friends
+    friendships,
+    friendRequests,
 })
