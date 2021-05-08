0ccfe8611bf31af497da6a7e0602b50e334355af
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-05-08 06:09:43 -0400

    created controllers

--- app/controllers/api/friends_controller.rb
+++ app/controllers/api/friends_controller.rb
@@ -1,2 +1,31 @@
 class Api::FriendsController < ApplicationController
+    def create
+        @friendship = Friendship.new(friendship_params)
+        friend_request = FriendRequest.find_by(receiver_id: params[:friendship][:user_id], requester_id: params[:friendship][:friend_id])
+        corresponding_friendship = Friendship.new({user_id: params[:friendship][:friend_id], friend_id: params[:friendship][:user_id]})
+        if @friendship.save
+            corresponding_friendship.save
+            friend_request.destroy
+            render :show
+        else 
+            render json: @friendship.errors.full_messages, status: 400
+        end
+    end
+
+    def destroy
+        @friendship = Friendship.find_by(id: params[:id])
+        corresponding_friendship = @friendship.corresponding_friendship
+        if @friendship
+            @friendship.destroy
+            corresponding_friendship.destroy
+            render :show 
+        else 
+            render json: ['Unable to find friendship'], status: 404 
+        end
+    end
+
+    def friendship_params
+        params.require(:friendship).permit(:user_id, :friend_id)
+    end
+
 end
