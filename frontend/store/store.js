8296324b95cf9ba9f79ccf6ea350c4a4dd3b30a2
jas-singh-code <jaspreetsingh7798@gmail.com>
2021-07-19 10:47:42 -0400

    fixed bug and remove logger from store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -7,6 +7,6 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default (preloadedState = {}) => createStore(
     rootReducer, preloadedState,
-    composeWithDevTools(applyMiddleware(thunk, logger))
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
